5d46f9f2071d87a01766c8d61558cdc95251aa56
Nick <nikmaunt@gmail.com>
2022-09-23 16:57:46 +0300

    Calculate average

---
+++ 8 kyu/5.js
@@ -0,0 +1,8 @@
+/* Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0 */.
+
+function findAverage(array) {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
+  
