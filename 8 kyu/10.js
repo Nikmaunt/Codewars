7b386ed0eb8fcc0ddbbbb5836776302b70a13435
Nick <nikmaunt@gmail.com>
2022-09-27 16:38:20 +0300

    Simple multiplication

---
+++ 8 kyu/10.js
@@ -0,0 +1,6 @@
+/* multiplying a given number by eight if it is an even number and by nine otherwise. */
+
+const simpleMultiplication = (number) => { 
+    return number % 2 == 0 ? number*8 : number*9;};
+  console.log(simpleMultiplication(3));
+  
