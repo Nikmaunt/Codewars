bfffd421acc19ac6da152f00bfbb0f517a93c5b0
Nick <nikmaunt@gmail.com>
2022-12-03 12:25:20 +0300

    Find the stray number

---
+++ 7 kyu/37.js
@@ -0,0 +1,10 @@
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+//Complete the method which accepts such an array, and returns that single different number.
+
+//The input array will always be valid! (odd-length >= 3)
+
+function stray(numbers) { 
+    let s = (numbers.filter(el=> numbers.indexOf(el) ))/1
+    return s;
+     }
