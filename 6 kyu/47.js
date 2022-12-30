3ba74671552882b6c1ac8b4106a27a3c2cee240a
Nick <nikmaunt@gmail.com>
2022-12-30 23:43:26 +0300

    Unique In Order

--- 6 kyu/47.js
+++ 6 kyu/47.js
@@ -2,15 +2,24 @@
  sequence and returns a list of items without any elements with 
  the same value next to each other and preserving the original order of elements.*/
 
-var uniqueInOrder=function(iterable){
-    let newArr = []
-      for (let i = 0; i < iterable.length; i++) {
-        let copy = iterable.split('')
-        if (copy[i] !== copy[i+1]) {
-          copy[i] = copy[i]
-          newArr.push(copy[i])
-        }
+ var uniqueInOrder=function(iterable){
+  let newArr = []
+    for (let i = 0; i < iterable.length; i++) {
+      if( typeof iterable === String){
+           let copy = iterable.split('')
       }
-      return newArr
-  }
-  console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+          else  copy = iterable
+      if (copy[i] !== copy[i+1]) {
+        copy[i] = copy[i]
+        newArr.push(copy[i])
+      }
+    }
+    return newArr
+}
+
+console.log(uniqueInOrder([1,2,3,1,1]))
+
+
+/*var uniqueInOrder=function(iterable){
+  return [...iterable].filter((a, i) => a !== iterable[i-1])
+}*/
