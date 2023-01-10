95c7ac85a00f2fc0b300f781be0e57a30f67bb42
Nick <nikmaunt@gmail.com>
2023-01-10 18:51:20 +0300

    Count the number of Duplicates

---
+++ 6 kyu/48.js
@@ -0,0 +1,13 @@
+/*Count the number of Duplicates
+Write a function that will return the
+ count of distinct case-insensitive alphabetic characters and numeric 
+ digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.*/
+
+
+
+function duplicateCount(text){
+    let count = text.toLowerCase().split('').map(el => 
+    text.toLowerCase().split(el).length - 1 > 1 ? el : '')
+    let res =  [...new Set(count)].join('').length
+    return res
+  }
